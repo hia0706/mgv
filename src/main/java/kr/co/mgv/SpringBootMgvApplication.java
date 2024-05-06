3d5aca0d6c8ca0c9306e8da31cc45ab7584fe59d
hia0706 <ljjs1215@gmail.com>
2024-05-06 20:49:11 +0900

    Feat 부트 app 코드 수정 및 pom 수정

--- src/main/java/kr/co/mgv/SpringBootMgvApplication.java
+++ src/main/java/kr/co/mgv/SpringBootMgvApplication.java
@@ -2,11 +2,20 @@ package kr.co.mgv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.swing.*;
 
 @SpringBootApplication
 @EnableScheduling
-public class SpringBootMgvApplication {
+public class SpringBootMgvApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMgvApplication.class, args);
